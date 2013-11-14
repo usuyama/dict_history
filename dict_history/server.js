0c6b7ea9e162f502f7ff05d2256a7055dbd2cba9
Naoto Usuyama <usuyama.naoto@gmail.com>
2013-11-15 03:21:01 +0900

    send queries to the server

---
+++ dict_history/server.js
@@ -0,0 +1,20 @@
+function parseJson(req, url) {
+  var jsonResponse = req.responseJSON;
+  console.log(jsonResponse);
+  if (req.status == 200) {
+    // registerd
+  } else if (req.status == 401) {
+    // need to login
+  }
+}
+
+sendToServer(location.href);
+function sendToServer(url) {
+  var req = new XMLHttpRequest();
+  req.overrideMimeType("application/json");
+  req.open("POST",SETTINGS['create_url'],true);
+  req.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+  var target = this;
+  req.onload  = function() { target.parseJson(req, url) };
+  req.send("url=" + url);
+}
